1462827c7a3b12f6cf56da93e4588c1867112c15
Steve De La Rosa <stevedelarosa1036@gmail.com>
2025-03-14 10:27:47 -0400

    optimized image and added service worker

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Card = ({ name, email, id }) => {
-  return (
-    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
-      <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    </div>
-  );
-}
+    return (
+        <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
+            <img
+                alt="robots"
+                src={`https://robohash.org/${id}?200x200&format=webp`}
+                width="200"
+                height="200"
+            />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+};
 
 export default Card;
